428373dc637f73643eda57b2561b7a96ee221bdc
designer38 <somayaelsayed1@gmail.com>
2021-08-27 05:23:01 +0200

    index

--- assets/js/app.js
+++ assets/js/app.js
@@ -179,6 +179,42 @@ jQuery(document).ready(function ($) {
       // instead of a settings object
     ]
   });
+  $('.responsive2').slick({
+    dots: true,
+    infinite: false,
+    arrows:true,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
   
     
     
