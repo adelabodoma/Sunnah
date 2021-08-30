c824474ebfaa7b00e03461fbc71fba5c51fb357b
designer38 <somayaelsayed1@gmail.com>
2021-08-30 06:08:38 +0200

    complete project

--- assets/js/app.js
+++ assets/js/app.js
@@ -151,6 +151,18 @@ jQuery(document).ready(function ($) {
       tabTrigger.show()
     })
   })
+
+
+  $(".toggle-password").click(function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
   $('.responsive').slick({
     dots: true,
     infinite: false,
@@ -173,6 +185,42 @@ jQuery(document).ready(function ($) {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.responsive3').slick({
+    dots: true,
+    infinite: false,
+    arrows:true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
