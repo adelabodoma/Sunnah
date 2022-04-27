e78a5e52a69b5542b8fcc1fc78b71f8143f9119d
Adel Sadek <adelabodoma@gmail.com>
2022-04-27 02:06:05 +0200

    add: fix responsive issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -176,14 +176,14 @@ jQuery(document).ready(function ($) {
     arrows: true,
     speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     adaptiveHeight: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -278,7 +278,7 @@ jQuery(document).ready(function ($) {
         breakpoint: 800,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToScroll: 1
         }
       },
       {
